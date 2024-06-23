3b4a2f3809f60f79b7cb20fc921571a9fcffced3
Nitin Nazeer <nitin.nazeer@gmail.com>
2024-06-23 03:00:11 -0400

    comment out comments ;)

--- app/lib/comments.ts
+++ app/lib/comments.ts
@@ -12,21 +12,21 @@ export async function selectComments(): Promise<Tables<'comments'>[] | null> {
     return data
 }
 
-export async function insertComment(formData: FormData) {
-    const supabase = createClient()
-    const { data: { user } } = await supabase.auth.getUser()
+// export async function insertComment(formData: FormData) {
+//     const supabase = createClient()
+//     const { data: { user } } = await supabase.auth.getUser()
 
-    const title = formData.get('title') as string
-    const body = formData.get('body') as string
-    if (user) {
-        await supabase.from("comments").insert({
-            post_id: ,
-            user_id: user.id,
-            body:,
-        })
-    }
-    revalidatePath('/posts')
-}
+//     const title = formData.get('title') as string
+//     const body = formData.get('body') as string
+//     if (user) {
+//         await supabase.from("comments").insert({
+//             post_id: ,
+//             user_id: user.id,
+//             body:,
+//         })
+//     }
+//     revalidatePath('/posts')
+// }
 
 export async function deletePost() {
     return;
